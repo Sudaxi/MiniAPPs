96e9dd8e7ae02dd21a768ff2aaefabd6894cdcce
大喜 <suyq1@homelink.com.cn>
2017-04-01 15:17:07 +0800

    整理结构

--- page/component/pages/index/index.js
+++ page/component/pages/index/index.js
@@ -6,12 +6,6 @@ Page({
     motto: 'Welcome To DaxiHome',
     userInfo: {}
   },
-    //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
     console.log('onLoad')
     var that = this
---
+++ page/component/pages/show/show.js
@@ -0,0 +1,16 @@
+Page({
+    setViews: function() {
+        console.log('setViews')
+  },
+  setScrollview: function() {
+console.log('setScrollview')
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
